635e4cfa8a548fca763a2b07c694ba884fb57991
alinayemelianova <yemelianova.alina@gmail.com>
2024-11-10 15:31:34 -0600

    Library Mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ['src/components'],
+    }),
+  ],
   build: {
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'src/components/index.ts'),
       name: 'FrontendMasters',
@@ -16,9 +25,9 @@ export default defineConfig({
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDom'
-        }
-      }
-    }
-  }
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
